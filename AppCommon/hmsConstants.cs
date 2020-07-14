703c5a8f79630c8f1816564bce3485df14ef31e3
Abdullah-onGit <abdullah.bsccs1997@gmail.com>
2020-07-14 15:19:44 +0530

    saniya login page integrated

--- AppCommon/hmsConstants.cs
+++ AppCommon/hmsConstants.cs
@@ -2,7 +2,7 @@ namespace hms
 {
     public class hmsConstants{
         //public const string ConnectionString = "Server=localhost;Port=5432;Database=hmsDB;User Name=postgres;Password=postgres;";
-        public const string ConnectionString = "Server=13.76.184.249;Integrated Security=false;Database=hmsDB;User Id=abu1;Password=abu1;";
+        public const string ConnectionString = "Server=13.76.172.132;Integrated Security=false;Database=hmsDB;User Id=SA;Password=VoidMain@124;";
     #region Session Constants
     
     public const string userName = "userName";
