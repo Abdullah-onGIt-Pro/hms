e616d44ebfde13f7e2eddeb7529fe3f3f3c88220
Abdullah-onGIt-Pro <67194702+Abdullah-onGIt-Pro@users.noreply.github.com>
2020-08-13 16:28:24 +0530

    Update hmsConstants.cs
    Update

--- AppCommon/hmsConstants.cs
+++ AppCommon/hmsConstants.cs
@@ -2,8 +2,8 @@ namespace hms
 {
     public class hmsConstants
     {
-        //public const string ConnectionString = "Server=localhost;Port=5432;Database=hmsDB;User Name=postgres;Password=postgres;";
-        public const string ConnectionString = "Server=13.76.172.132;Integrated Security=false;Database=hmsDB;User Id=SA;Password=VoidMain@124;";
+        public const string ConnectionString = "Server=localhost;Port=5432;Database=hmsDB;User Name=postgres;Password=postgres;";
+        
         #region Session Constants
         public const string loginUserAutoId = "loginUserAutoId";
         public const string userName = "userName";
